356342a277c22f2d5b4bec4f488ae54066f13655
RekoAlrasheed <ralrasheed@sandiego.edu>
2025-11-06 23:32:26 +0300

    refactor: final cleanup - remove unused exports and optimize server components

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
@@ -29,4 +33,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig 
+module.exports = withBundleAnalyzer(nextConfig) 
